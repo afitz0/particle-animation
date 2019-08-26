d16a3d07871e4d881cbf49624c9069ec2f14757a
Fitz <afitzgibbon@google.com>
2019-08-26 16:41:04 -0700

    Remove needless getters/setters

--- lib/star.dart
+++ lib/star.dart
@@ -1,25 +1,7 @@
 import 'dart:math';
 
 class Star {
-  double _mass;
-  Point<double> _velocity;
-  Point<double> _location;
-
-  double get mass => _mass;
-
-  set mass(double mass) {
-    _mass = mass;
-  }
-
-  Point<double> get location => _location;
-
-  set location(Point<double> location) {
-    _location = location;
-  }
-
-  Point<double> get velocity => _velocity;
-
-  set velocity(Point<double> velocity) {
-    _velocity = velocity;
-  }
+  double mass;
+  Point<double> velocity;
+  Point<double> location;
 }
